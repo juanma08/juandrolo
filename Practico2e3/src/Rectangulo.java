fff3093c9100474a5265322ade0789bb9321d420
Casso <juanmacassoli@gmail.com>
2020-05-09 20:42:38 -0300

    se logro la creacion de el objeto rectangulo EJ3

---
+++ Practico2e3/src/Rectangulo.java
@@ -0,0 +1,28 @@
+
+public class Rectangulo {
+	
+	double base;
+	double altura;
+	
+	public Rectangulo(int x, int y){
+		base = x;
+		altura =y;
+	}
+
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+}
--- Practico2e3/src/admin.java
+++ Practico2e3/src/admin.java
@@ -5,8 +5,12 @@ public class admin extends punto_geometrico {
 		punto_geometrico p= new punto_geometrico();
 		System.out.println(p.getX());
 		System.out.println(p.getY());
-		p.desplazar(40, 64);
-		System.out.println(p.distancia_eu(20, 24));
+		p.setX(10);
+		p.setY(20);
+		int x =p.getX();
+		int y =p.getY();
+		Rectangulo r = new Rectangulo (x,y);	
+		System.out.println(r.getBase()+ " " + r.getAltura());
 	}
 
 }
